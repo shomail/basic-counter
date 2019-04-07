efbc0472e18a400057e6395c45509e73b92d735b
Shomail <shomail1988@gmail.com>
2019-04-07 22:01:34 +0200

    basic counter working

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
   render() {
+    const {counter} = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {counter}</h1>
+        <button onClick={() => this.setState({counter: counter + 1})} className="full-width">Increment</button>
+        <button onClick={() => this.setState({counter: counter - 1})} className="full-width">Decrement</button>
+        <button onClick={() => this.setState({counter: 0})} className="full-width">Reset</button>
       </section>
     );
   }
