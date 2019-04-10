b39b323b72937d16950449c1006aed26d495c423
Shomail <shomail1988@gmail.com>
2019-04-10 15:54:53 +0200

    render props pattern

--- src/Application.js
+++ src/Application.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
-// import Counter from './Counter';
-import CounterContainer from './CounterContainer';
-// import WithCount from './WithCount';
+import Counter from './Counter';
+// import CounterContainer from './CounterContainer';
+import WithCount from './WithCount';
 
 export default class Application extends Component {
   render() {
     return (
       <main className="Application">
-        <CounterContainer />
+        {/* <CounterContainer /> */}
+        <WithCount
+          render={
+            (count, onIncrement, onDecrement, onReset) => (
+              <Counter
+                count={count}
+                onIncrement={onIncrement}
+                onDecrement={onDecrement}
+                onReset={onReset}
+              />
+            )
+          }
+        />
       </main>
     );
   }
--- src/WithCount.js
+++ src/WithCount.js
@@ -1,1 +1,32 @@
-// Intentionally left blank.
+import React, {Component} from 'react';
+
+export default class WithCount extends Component {
+  state = {counter: 0}
+
+  handleIncrement = () => {
+    this.setState({counter: this.state.counter + 1})
+  }
+
+  handleDecrement = () => {
+    this.setState({counter: this.state.counter - 1})
+  }
+
+  handleReset = () => {
+    this.setState({counter: 0})
+  }
+
+  render() {
+    return (
+      <div className="WithCount">
+        {
+          this.props.render(
+            this.state.counter,
+            this.handleIncrement,
+            this.handleDecrement,
+            this.handleReset,
+          )
+        }
+      </div>
+    )
+  }
+}
